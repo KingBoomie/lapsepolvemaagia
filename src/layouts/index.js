75ab6a6e7cd3d9a9a5d22415611761d5ea64756e
Kristjan Laht <kristjan.laht13@gmail.com>
2018-05-30 12:26:22 +0300

    fingerprint fix2

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,7 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 
-// HACK: webpack error when building otherwise
+
+import { css } from 'emotion';
+
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import { request } from '../utils/api.js';
+
+import icon from '../icon.png'
+
+
+class TemplateWrapper extends React.Component {
+
+  componentDidMount() {
+    // HACK: webpack error when building otherwise
 try {
   Fingerprint2 = require('fingerprintjs2');
 
@@ -30,49 +43,45 @@ try {
 } catch (e) {
   console.log(e)
 }
-import { css } from 'emotion';
+  }
 
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import { request } from '../utils/api.js';
+  render() {
+    return (
+      <div>
+        <Helmet
+          title="Lapsepolvemaagia"
+          meta={[
+            {
+              name: 'description',
+              content: 'Veebileht suvisele lapsepõlvemaagia festivalile',
+            },
+            {
+              name: 'keywords',
+              content:
+                'lapsepõlv, maagia, festival, haapsalu, lapsepõlvemaagia ,lapsepolvemaagia',
+            },
+          ]}
+          link={[{ rel: 'shortcut icon', type: 'image/png', href: `${icon}` }]}
+        />
+        <Header />
+    
+        <div
+          className={css({
+            margin: '0 auto',
+            maxWidth: 1280,
+            padding: '1rem 1.0875rem 1.45rem',
+          })}
+        >
+          {props.children()}
+        </div>
+    
+        <Footer />
+      </div>
+    )
+  }
+}
 
-import icon from '../icon.png'
-
-
-
-
-const TemplateWrapper = ({ children }) => (
-  <div>
-    <Helmet
-      title="Lapsepolvemaagia"
-      meta={[
-        {
-          name: 'description',
-          content: 'Veebileht suvisele lapsepõlvemaagia festivalile',
-        },
-        {
-          name: 'keywords',
-          content:
-            'lapsepõlv, maagia, festival, haapsalu, lapsepõlvemaagia ,lapsepolvemaagia',
-        },
-      ]}
-      link={[{ rel: 'shortcut icon', type: 'image/png', href: `${icon}` }]}
-    />
-    <Header />
-
-    <div
-      className={css({
-        margin: '0 auto',
-        maxWidth: 1280,
-        padding: '1rem 1.0875rem 1.45rem',
-      })}
-    >
-      {children()}
-    </div>
-
-    <Footer />
-  </div>
-);
+const TemplateWrapper = ({ children }) => ;
 
 TemplateWrapper.propTypes = {
   children: PropTypes.func
