4e81f65ea3e100bba05ff9f8225cbde1e42d65c2
Kristjan Laht <kristjan.laht13@gmail.com>
2018-05-30 12:22:54 +0300

    fix backendURL

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ import openSocket from 'socket.io-client';
 import Axios from 'axios'
 
 console.log(process.env)
-const backendURL = process.env.URL !== undefined 
+const backendURL = process.env.NODE_ENV === "production" 
 ? "https://lit-hollows-38242.herokuapp.com"
 : "http://localhost:5000"
 
